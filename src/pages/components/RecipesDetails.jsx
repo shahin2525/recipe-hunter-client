92dcc841fd571b0e814b48cf3c03512b71fcd45b
shahin2525 <shahin451999@gmail.com>
2023-06-04 09:57:01 -0700

    data show in viewDetails

---
+++ src/pages/components/RecipesDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecipesDetails = () => {
+  return (
+    <div>
+      <h1>I am recipes details:</h1>
+    </div>
+  );
+};
+
+export default RecipesDetails;
--- src/pages/components/ViewDetails.jsx
+++ src/pages/components/ViewDetails.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom";
 
 const ViewDetails = () => {
   const chef = useLoaderData();
+  console.log(chef);
   const {
     chefName,
     chefImageURL,
@@ -10,24 +11,32 @@ const ViewDetails = () => {
     shortBio,
     numOfRecipes,
     yearsOfExperience,
+    recipes,
   } = chef;
   console.log(chef);
   return (
-    <div className="bg-red-200">
-      <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="bg-red-200  px-10 py-5">
+      <div className="card w-auto bg-red-100 shadow-xl my-4 mx-auto">
         <figure>
           <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            className="rounded-3xl pt-5 pb-3 mt-5"
+            src={chefImageURL}
             alt="Shoes"
           />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+        <div className="card-body ml-24 pl-20 font-bold">
+          <h2 className="card-title text-2xl font-bold">Name: {chefName}</h2>
+          <p>Short-Bio-Data: {shortBio}</p>
+          <p>Likes: {numOfLikes}</p>
+          <p>Recipes: {numOfRecipes}</p>
+          <p>Year Of Experience: {yearsOfExperience}</p>
         </div>
+      </div>
+      <h1 className="text-center font-bold text-3xl">My Recipes</h1>
+      <div>
+        {recipes.map((recipe) => (
+          <p></p>
+        ))}
       </div>
     </div>
   );
