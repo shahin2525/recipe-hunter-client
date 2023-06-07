e22f8b1c1a64c1eda27ed0a8da11f8cd718d80ee
shahin2525 <shahin451999@gmail.com>
2023-06-06 21:00:18 -0700

    firbase hide config added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+console.log("cenvironment variable", import.meta.env.VITE_apiKey);
 const firebaseConfig = {
-  apiKey: "AIzaSyBI7mk0xrJMCc0B96RMYCsjigClexU9t5I",
-  authDomain: "recipe-garden-client.firebaseapp.com",
-  projectId: "recipe-garden-client",
-  storageBucket: "recipe-garden-client.appspot.com",
-  messagingSenderId: "1078790675324",
-  appId: "1:1078790675324:web:8ac9c1c07b42f4d39fe2f9",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
